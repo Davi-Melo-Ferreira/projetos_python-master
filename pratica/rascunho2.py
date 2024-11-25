f23cf19f4bdeb8659de3ab911da130b3542e3a33
Davi-Melo-Ferreira <davidemeloferreira13@gmail.com>
2024-11-25 16:29:55 -0300

    aulas

--- pratica/rascunho2.py
+++ pratica/rascunho2.py
@@ -3,10 +3,3 @@ import os
 
 os.system('cls')
 
-lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-
-for i in lista:
-    if i % 2 == 0:
-        if i % 3 == 0:
-            if i % 4 == 0:
-                print(i)
