009c67a2278d3cb8bcbfe4f3548e3724ffe52749
Davi-Melo-Ferreira <davidemeloferreira13@gmail.com>
2025-01-24 13:38:05 -0300

    a

--- pratica/rascunho.py
+++ pratica/rascunho.py
@@ -1,74 +1,28 @@
-# Faça um programa de perguntas e respostas sobre os estados e capitais brasileiras.
-# O programa deverá exibir para o usuário o estado e perguntar qual a sua capital.
-# Se o usuário errar a resposta, o programa será finalizado apresentando quantas perguntas estão corretas.
+# F) Crie uma função que receba 2 listas: 
+# - lista 1: nome, peso, idade 
+# - lista 2: John, 40, 1.8
+# - Sua função deverá criar um dicionário contendo 
+# chave e valor utilizando como base essas duas listas. 
+# Depois, seu programa deverá imprimir esse dicionário 
+# utilizando uma estrutura de repetição for.
+
 import os
-import random
+
 
 os.system('cls')
+dicionario = {}
+def receber_dicionario(lista_1, lista_2):
+    for chave, valor in zip(lista_1, lista_2):
+        dicionario[chave] = valor
 
-# Lista de estados do Brasil
-estados_capitais = {
-    'Acre': 'Rio Branco',
-    'Alagoas': 'Maceió',
-    'Amapá': 'Macapá',
-    'Amazonas': 'Manaus',
-    'Bahia': 'Salvador',
-    'Ceará': 'Fortaleza',
-    'Espírito Santo': 'Vitória',
-    'Goiás': 'Goiânia',
-    'Maranhão': 'São Luís',
-    'Mato Grosso': 'Cuiabá',
-    'Mato Grosso do Sul': 'Campo Grande',
-    'Minas Gerais': 'Belo Horizonte',
-    'Pará': 'Belém',
-    'Paraíba': 'João Pessoa',
-    'Paraná': 'Curitiba',
-    'Pernambuco': 'Recife',
-    'Piauí': 'Teresina',
-    'Rio de Janeiro': 'Rio de Janeiro',
-    'Rio Grande do Norte': 'Natal',
-    'Rio Grande do Sul': 'Porto Alegre',
-    'Rondônia': 'Porto Velho',
-    'Roraima': 'Boa Vista',
-    'Santa Catarina': 'Florianópolis',
-    'São Paulo': 'São Paulo',
-    'Sergipe': 'Aracaju',
-    'Tocantins': 'Palmas'
-}
+    return dicionario
 
-invicto = True
 
-while invicto == True:
-    acertos = 0
-    def randomizar_estados(dicionario):
-        itens = list(dicionario.items())
-        random.shuffle(itens)
-        estado_capital = dict(itens)
-        return estado_capital
+lista_1 = ['nome', 'peso', 'idade']
+lista_2 = ['john', 40, 18]
 
-    
-    estado_capital = randomizar_estados(estados_capitais)
-    
-    for chave, valor in estado_capital.items():
-        pergunta = input(f'Qual a capital de {chave}?(): ').lower()
-        if pergunta == valor.lower():
-            acertos += 1
-            print('Parabés, Você acertou!!')
-            input('Pressione qualquer tecla para a próxima pergunta...')
-            os.system('cls')
-        else:
-            print('Você Errou!!!')
-            input('Pressione qualquer tecla para sair...')
-            os.system('cls')
-            break
-    invicto = False
 
-qntd_perguntas = len(estado_capital)
-if acertos == qntd_perguntas:
-    print('Parabéns, Você venceu!!!')
-else:
-    print('Mais sorte da próxima vez...')
-print('-'*70)
-print(f'Você acertou a capital de {acertos} estados')
-print('-'*70)
-print(f'Resultado: {acertos}/{qntd_perguntas}\n')
+dicionario = receber_dicionario(lista_1, lista_2)
+
+for i, j in dicionario.items():
+    print(f'{i}: {j}')
