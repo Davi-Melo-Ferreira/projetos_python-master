1ca52415fbd9ad3d83bc21f3765cfb92f86d9678
Davi-Melo-Ferreira <davidemeloferreira13@gmail.com>
2025-02-13 16:38:15 -0300

    metodos acessores

--- POO/aula18_poo/Atividades/Atividade 001/a.py
+++ POO/aula18_poo/Atividades/Atividade 001/a.py
@@ -2,3 +2,59 @@
 Faça um programa que peça 3 valores,
 depois calcule e imprima  a soma e a multiplicação desses valores
 '''
+import os
+
+class Equacao:
+    def __init__(self, a, b, c):
+        # Chama o setter para definir o valor a
+        self.set_a(a)
+        # Chama o setter para definir o valor b
+        self.set_b(b)
+        # Chama o setter para definir o valor c
+        self.set_c(c)
+
+    def get_a(self):
+        return self._a
+    
+    def set_a(self, a):
+        self._a = a
+    
+    def get_b(self):
+        return self._b
+    
+    def set_b(self, b):
+        self._b = b
+    
+    def get_c(self):
+        return self._c
+    
+    def set_c(self, c):
+        self._c = c
+    
+    def calcular_soma(self): 
+        self._somatorio = self._a + self._b + self._c
+    
+    def get_somatorio(self):
+        self.calcular_soma()
+        return self._somatorio
+    
+    def calcular_multiplicacao(self):
+        self._multiplicado = self._a * self._b * self._c
+    
+    def get_multiplicacao(self):
+        self.calcular_multiplicacao()
+        return self._multiplicado
+
+    def printar(self):
+        print(f'A soma dos valores {self._a} + {self._b} + {self._c} = {self.get_somatorio()}')
+        print(f'A multiplicação dos valores {self._a} x {self._b} x {self._c} = {self.get_multiplicacao()}')
+    
+os.system('cls')
+
+a = int(input('Digite o valor de a: '))
+b = int(input('Digite o valor de b: '))
+c = int(input('Digite o valor de c: '))
+
+equacao = Equacao(a, b, c)
+
+equacao.printar()
--- POO/aula18_poo/Atividades/Atividade 001/b.py
+++ POO/aula18_poo/Atividades/Atividade 001/b.py
@@ -1,3 +1,15 @@
 '''
 Faça um programa que peça o ano do seu nascimento e calcule a sua idade.
 '''
+import os
+import datetime
+
+class Pessoa:
+    def __init__(self, ano_nascimento):
+        
+        self.set_ano_nascimento(ano_nascimento)
+    
+    def calcular_idade(self):
+        ano_atual = datetime.now()
+        
+        pass
